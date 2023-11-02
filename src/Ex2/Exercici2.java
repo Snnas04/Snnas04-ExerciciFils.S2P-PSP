4709005b429762b46da5ef1b353c49c2b3ae20f6
Marc <marcsans26@gmail.com>
2023-11-02 17:20:18 +0100

    exercici 2

--- src/Ex2/Exercici2.java
+++ src/Ex2/Exercici2.java
@@ -1,39 +1,49 @@
 package Ex2;
 
-public class Exercici2 implements Runnable {
+import java.util.Arrays;
 
-    private Comptes xobj;
+public class Exercici2 extends Thread {
+    // queremos que "compte" cambié de valor cada vez que se realiza una operacion, y que el siguiente hilo coja el valor de "compte" que ha dejado el anterior hilo
+    private int operacio;
+    private int resultat;
+    private static int compte = 990;
 
-    public Exercici2(Comptes m) {
-        xobj=m;
+    public Exercici2(int operacio) {
+        this.operacio = operacio;
     }
 
-    public void run(){
-        int valInicial = 100;
-        xobj.setSuma(valInicial);
-        xobj.setResta(valInicial);
-        System.out.println("Tirada fil " + Thread.currentThread().getName() + ": " + valInicial);
+    public void run() {
+        try {
+            if (Thread.currentThread().getName().equals("Suma")) {
+                resultat = compte + operacio;
+                compte = resultat;
+                System.out.println("Suma compte: " + resultat);
+            } else if (Thread.currentThread().getName().equals("Resta")) {
+                resultat = compte - operacio;
+                compte = resultat;
+                System.out.println("Resta compte: " + resultat);
+            }
+        } catch (Exception e) {
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        Comptes ans=new Comptes(0);
-
-        Exercici2 obj1 = new Exercici2(ans);
-        Exercici2 obj2 = new Exercici2(ans);
+        Exercici2 obj1 = new Exercici2(10);
+        Exercici2 obj2 = new Exercici2(20);
 
         Thread fil_1 = new Thread(obj1);
         fil_1.setName("Suma");
+
         Thread fil_2 = new Thread(obj2);
         fil_2.setName("Resta");
 
         fil_1.start();
         fil_2.start();
 
-         fil_1.join(); //Espera el fil_1 que el fil principal, el que l'ha invocat acabi
-         fil_2.join();
+        fil_1.join();
+        fil_2.join();
 
-        System.out.println("Total tirada: "+ ans.getSuma());
+        System.out.println("Total: " + compte);
         System.out.println("Final Fil Principal");
-
     }
 }
