4fe69f658830355c7f0bb5418537e9d8c46e72a6
Marc <marcsans26@gmail.com>
2023-11-07 18:19:56 +0100

    activitat 4

--- src/Ex2/Exercici2.java
+++ src/Ex2/Exercici2.java
@@ -1,9 +1,26 @@
 package Ex2;
 
-import java.util.Arrays;
-
 public class Exercici2 extends Thread {
-    // queremos que "compte" cambié de valor cada vez que se realiza una operacion, y que el siguiente hilo coja el valor de "compte" que ha dejado el anterior hilo
+    public static void main(String[] args) throws InterruptedException {
+        Exercici2 obj1 = new Exercici2(10);
+        Exercici2 obj2 = new Exercici2(20);
+
+        Thread fil_1 = new Thread(obj1);
+        fil_1.setName("Suma");
+
+        Thread fil_2 = new Thread(obj2);
+        fil_2.setName("Resta");
+
+        fil_1.start();
+        fil_2.start();
+
+        fil_1.join();
+        fil_2.join();
+
+        System.out.println("Total: " + compte);
+        System.out.println("Final Fil Principal");
+    }
+
     private int operacio;
     private int resultat;
     private static int compte = 990;
@@ -25,25 +42,5 @@ public class Exercici2 extends Thread {
             }
         } catch (Exception e) {
         }
-    }
-
-    public static void main(String[] args) throws InterruptedException {
-        Exercici2 obj1 = new Exercici2(10);
-        Exercici2 obj2 = new Exercici2(20);
-
-        Thread fil_1 = new Thread(obj1);
-        fil_1.setName("Suma");
-
-        Thread fil_2 = new Thread(obj2);
-        fil_2.setName("Resta");
-
-        fil_1.start();
-        fil_2.start();
-
-        fil_1.join();
-        fil_2.join();
-
-        System.out.println("Total: " + compte);
-        System.out.println("Final Fil Principal");
     }
 }
--- src/Ex4/Exercici4.java
+++ src/Ex4/Exercici4.java
@@ -1,5 +1,53 @@
 package Ex4;
 
-public class Exercici4 {
+import java.util.Scanner;
 
+public class Exercici4 extends Thread {
+    public static void main(String[] args) {
+        Fibonacci(50);
+    }
+
+    private final int n;
+
+    public Exercici4(int n) {
+        this.n = n;
+    }
+
+    public static void Fibonacci(int n) {
+        Scanner sc = new Scanner(System.in);
+
+        Exercici4 obj1 = new Exercici4(n);
+        Thread fil_1 = new Thread(obj1);
+        fil_1.start();
+
+        String exit = sc.nextLine();
+        if (exit.equals("exit")) {
+            fil_1.interrupt();
+            System.out.println("Fil interrumpit");
+        }
+
+        sc.close();
+    }
+
+    public void run() {
+        int num1 = 0;
+        int num2 = 1;
+        int suma = 0;
+
+        System.out.println(1);
+
+        while (suma < n) {
+            suma = num1 + num2;
+            num1 = num2;
+            num2 = suma;
+            System.out.println(suma);
+
+            try {
+                Thread.sleep(1500);
+            } catch (InterruptedException e) {
+                System.out.println("Proces finalitzat");
+                break;
+            }
+        }
+    }
 }
