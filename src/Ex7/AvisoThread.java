11aaf3912cac4b1abd71c425f80471af57be7b7b
Marc <marcsans26@gmail.com>
2023-11-14 19:42:07 +0100

    act 7

--- src/Ex7/AvisoThread.java
+++ src/Ex7/AvisoThread.java
@@ -14,16 +14,22 @@ class AvisoThread extends Thread {
         while (countdownThread.getTiempoRestante() >= 0) {
             int tiempoRestante = countdownThread.getTiempoRestante();
 
-            if (tiempoRestante < tiempoTotal/4) {
-                System.out.println("Queda 3/4");
-            } else if (tiempoRestante == tiempoTotal/2) {
-                System.out.println("Queda la mitad");
-            } else if (tiempoRestante <= tiempoTotal/4) {
-                System.out.println("Queda 1/3");
-            } else if (tiempoRestante == 0 * tiempoTotal) {
+            var mitad = tiempoTotal/2;
+            var cuarto = (tiempoTotal/4) + 1;
+            var tresCuartos = ((tiempoTotal/4)*3) + 1;
+
+            if (tiempoRestante == 0 * tiempoTotal) {
                 System.out.println("TIMEOUT");
             }
-
+            else if (tiempoRestante <= cuarto && cuarto - tiempoRestante <= 1 && cuarto - tiempoRestante >= 1) {
+                System.out.println("Queda 1/4");
+            }
+            else if (tiempoRestante == mitad) {
+                System.out.println("Queda la mitad");
+            }
+            else if (tiempoRestante >= tresCuartos && tiempoRestante - tresCuartos <= 1 && tiempoRestante - tresCuartos >= 1) {
+                System.out.println("Queda 3/4");
+            }
             try {
                 sleep(1000); // Simula un segundo de espera
             } catch (InterruptedException e) {
